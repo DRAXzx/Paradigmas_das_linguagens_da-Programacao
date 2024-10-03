ad2b4fce36754ec1288e95dbc91cb41cf2639ebf
Arione <102837312+DRAXzx@users.noreply.github.com>
2024-10-02 23:53:40 -0300

    Add files via upload

---
+++ Calibracao80k/src/CalibracaoThreads.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class CalibracaoThreads extends Thread {
+    private List<String> listaPalavras;
+    private int somaParcial;
+
+    public CalibracaoThreads(List<String> listaPalavras) {
+        this.listaPalavras = listaPalavras;
+        this.somaParcial = 0;
+    }
+
+    @Override
+    public void run() {
+        for (String palavra : listaPalavras) {
+            Integer primeiroDigito = null;
+            Integer ultimoDigito = null;
+
+            for (char c : palavra.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    if (primeiroDigito == null) {
+                        primeiroDigito = Character.getNumericValue(c);
+                    }
+                    ultimoDigito = Character.getNumericValue(c);
+                }
+            }
+
+            if (primeiroDigito != null && ultimoDigito != null) {
+                int numeroCalibracao = Integer.parseInt("" + primeiroDigito + ultimoDigito);
+                somaParcial += numeroCalibracao;
+            }
+        }
+    }
+
+    public int getSomaParcial() {
+        return somaParcial;
+    }
+}
+
+
